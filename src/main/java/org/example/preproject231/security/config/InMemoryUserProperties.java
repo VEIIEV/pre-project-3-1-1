9977f9fdaf75a80d58014b650a79cd4c2d1d07ce
Боткин_Кирилл <worldloflwarcraft@mail.ru>
2025-02-20 18:41:19 +0400

    починил чекбоксы

---
+++ src/main/java/org/example/preproject231/security/config/InMemoryUserProperties.java
@@ -0,0 +1,23 @@
+package org.example.preproject231.security.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.security.user")
+public class InMemoryUserProperties extends SecurityProperties.User {
+
+    private List<String> authorities = new ArrayList<>();
+
+    public List<String> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<String> authorities) {
+рты         this.authorities = authorities;
+    }
+}
